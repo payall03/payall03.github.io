7aec191ad16edbcbc56efdf625c4c2e049fc63f0
PayalV0 <payalverma66256@gmail.com>
2025-06-10 23:11:43 +0530

    My portfolio site

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
